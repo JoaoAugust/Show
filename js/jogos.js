9ad5865903f36c8f9f40aacb089b2e262af3da49
JoaoAugust <71889097+JoaoAugust@users.noreply.github.com>
2021-01-21 21:51:07 -0300

    Add files via upload

--- js/jogos.js
+++ js/jogos.js
@@ -1,3 +1,188 @@
-$(document).ready(function(){
-$("quest4").button(function(e))
-});
+var pt = 10;
+function pergunta1(){
+    let a1 = document.getElementById("a1").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (a1 == true ){
+        document.getElementById("resp1").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp1").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta2(){
+    let d2 = document.getElementById("d2").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (d2 == true ){
+        document.getElementById("resp2").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp2").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta3(){
+    let i1 = document.getElementById("i1").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (i1 == true ){
+        document.getElementById("resp3").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp3").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta4(){
+    let j1 = document.getElementById("j1").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (j1== true ){
+        document.getElementById("resp4").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp4").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta5(){
+    let bb= document.getElementById("bb").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+    if ( bb == true ){
+        document.getElementById("resp5").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp5").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta6(){
+    let ee= document.getElementById("ee").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (ee == true ){
+        document.getElementById("resp6").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp6").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta7(){
+    let hh = document.getElementById("hh").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (hh== true ){
+        document.getElementById("resp7").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp7").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta8(){
+    let ll = document.getElementById("ll").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (ll == true ){
+        document.getElementById("resp8").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp8").textContent = resultado2
+    }
+}
+var pt = 100;
+function pergunta9(){
+    let oo = document.getElementById("oo").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (oo == true ){
+        document.getElementById("resp9").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 100
+    }
+    else{
+        document.getElementById("resp9").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta10(){
+    let qq = document.getElementById("qq").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (qq == true ){
+        document.getElementById("resp10").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp10").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta11(){
+    let vv = document.getElementById("vv").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (vv == true ){
+        document.getElementById("resp11").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp11").textContent = resultado2
+    }
+}
+var pt = 10;
+function pergunta12(){
+    let yy = document.getElementById("yy").checked
+    let resultado = "Seu tempo esta sendo bem usado"
+    let resultado2 = "Te falta tempo de tela"
+
+    if (yy == true ){
+        document.getElementById("resp12").textContent = resultado
+        document.getElementById("result").textContent = pt
+        pt = pt + 10
+    }
+    else{
+        document.getElementById("resp12").textContent = resultado2
+    }
+}
+
+
+
+
+    
+
+
+
+
